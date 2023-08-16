0ebb6e80aaecf7a6720e42be3e9b99a7a6fdbbe1
alexlottrup <alexlottrup@gmail.com>
2023-08-16 13:38:35 +0200

    with manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.png'],
+			manifest: {
+				name: 'Troldspejlet',
+				short_name: 'Troldspejlet',
+				description: 'Et magisk spejl',
+				theme_color: '#8a437a',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
+		})
+	]
 })
