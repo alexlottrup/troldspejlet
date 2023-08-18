5853592ca78535ef706a3c1c8c55ae93df0f017f
alexlottrup <alexlottrup@gmail.com>
2023-08-18 21:59:56 +0200

    frame fit

--- src/components/frame/Frame.tsx
+++ src/components/frame/Frame.tsx
@@ -3,7 +3,9 @@ import styles from './Frame.module.scss'
 const Frame = () => {
 	return (
 		<div className={styles.frame}>
-			<div className={styles.ripple}></div>
+			<div className={styles.fit}>
+				<div className={styles.ripple}></div>
+			</div>
 		</div>
 	)
 }
